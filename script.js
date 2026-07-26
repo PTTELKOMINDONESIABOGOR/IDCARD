bf08da0ba40e84c0804bab242228d7a36fb13b30
PT. TELKOM INDONESIA/INDIHOME <ganzfriedsam@gmail.com>
2026-07-26 16:28:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+window.addEventListener("load", function () {
 
+    const loading = document.getElementById("loading");
+    const card = document.getElementById("card");
+
+    setTimeout(function () {
+
+        loading.style.display = "none";
+
+        card.classList.add("show");
+
+    }, 2000);
+
+});
